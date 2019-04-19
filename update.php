066870f3053956dce5d4d9076c243ab7858fd9e3
ewooonk <ewoutoonk@gmail.com>
2019-04-19 11:42:51 +0200

    Update update.php
    Added freebike_status

--- update.php
+++ update.php
@@ -12,12 +12,14 @@ require $_SERVER['DOCUMENT_ROOT'] . "/gbfs/en/system_information.php";
 require $_SERVER['DOCUMENT_ROOT'] . "/gbfs/en/station_information.php";
 require $_SERVER['DOCUMENT_ROOT'] . "/gbfs/en/station_status.php";
 require $_SERVER['DOCUMENT_ROOT'] . "/gbfs/en/system_pricing_plans.php";
+require $_SERVER['DOCUMENT_ROOT'] . "/gbfs/en/free_bike_status.php";
 
 gbfs();
 system_information ();
 station_information();
 station_status();
 system_pricing_plans();
+freebike_status();
 
 
 echo "succes";
